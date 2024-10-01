61e018a408008d5206fc28326e9c42171825f5c1
Alexander Bris <alexbris21@gmail.com>
2024-10-01 15:14:15 -0300

    deploy-2

--- src/db/database.js
+++ src/db/database.js
@@ -2,6 +2,7 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 const { Sequelize } = require('sequelize');
 
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
