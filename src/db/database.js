ecc112e48267ee56aaca644c6da9d679045afb10
Alexander Bris <alexbris21@gmail.com>
2024-10-01 15:57:21 -0300

    deploy-4

--- src/db/database.js
+++ src/db/database.js
@@ -12,6 +12,12 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
       rejectUnauthorized: false, // Para evitar errores de SSL en algunas configuraciones
     },
   },
+  pool: {
+    max: 5, // Número máximo de conexiones
+    min: 0, // Número mínimo de conexiones
+    acquire: 30000, // Tiempo máximo para obtener una conexión
+    idle: 10000 // Tiempo que una conexión puede estar inactiva antes de ser liberada
+  },
   protocol: 'postgres',
   logging: false,
 })
